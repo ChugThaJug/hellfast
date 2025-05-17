93b9b626d3a5ac777e6437c7f92f85038ca3fd62
thebrandedman <40077401+ChugThaJug@users.noreply.github.com>
2025-05-17 15:27:56 +0500

    buil7

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,16 +1,20 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess({
-    typescript: {
-      // TypeScript options
-    }
-  }),
-  kit: {
-    adapter: adapter()
-  }
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: 'index.html',
+			precompress: false,
+			strict: true
+		})
+	}
 };
 
 export default config;
